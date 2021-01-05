c52c41c697ee9ea57360cea4bcae7c26cdc0fc0d
Dominic Klaic <KlaicD.github.com>
2021-01-05 13:58:14 +0200

    running function

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8] //16 
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9] //16
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5] //16
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6] //16
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]    
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
 
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
@@ -49,11 +49,9 @@ const validateCred = array => {
      
   total = doubled + single + checkDigit;
   if ((total % 10) == 0 ){
-    console.log("valid");
     return true;
     
   } else {
-    console.log("invalid");
     return false;
   };
 }
@@ -66,6 +64,9 @@ const findInvalidCards = nestedArray => {
     } else {
       invalidCards.push(nestedArray[i]);
     }
-  };  
+  }; 
+  return ` Valid Cards: ${validCards}`;
 }
+
+findInvalidCards(batch);
 
